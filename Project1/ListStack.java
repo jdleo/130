fcc366e48a41322fdcfeceae141f35b65419a421
John Leonardo <j@Johns-MacBook-Pro.local>
2018-02-11 20:15:15 -0800

    moving files to individual folders

---
+++ Project1/ListStack.java
@@ -0,0 +1,19 @@
+public class ListStack implements DStack {
+    
+    public boolean isEmpty() {
+        
+    }
+    
+    public void push(double d) {
+        
+    }
+    
+    public double pop() {
+        
+    }
+    
+    public double peek() {
+        
+    }
+
+}
