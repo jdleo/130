7ed98e636616485bf46425ef5fab11e3a952a4c3
John Leonardo <j@Johns-MacBook-Pro.local>
2018-02-11 20:13:58 -0800

    dstack implementation

---
+++ ArrayStack.java
@@ -0,0 +1,19 @@
+public class ArrayStack implements DStack {
+    
+    public boolean isEmpty() {
+        
+    }
+    
+    public void push(double d) {
+        
+    }
+    
+    public double pop() {
+        
+    }
+    
+    public double peek() {
+        
+    }
+
+}
