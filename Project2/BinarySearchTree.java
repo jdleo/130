dd770a248a1f0e2c757f3d4706b7ff3dfd0296ae
John Leonardo <jdleonardo5@icloud.com>
2018-03-01 18:17:06 -0800

    getRoot and height methods

--- Project2/BinarySearchTree.java
+++ Project2/BinarySearchTree.java
@@ -5,7 +5,24 @@
  */
 
 public class BinarySearchTree<Integer> {
+    private Node root;      //root of BST
+    private int height;     //height of BST
     
+    /**
+     * Method for getting the root of this BST
+     * @return root : returns the root of this BST
+     */
+    public Node getRoot() {
+        return root;
+    }
+    
+    /**
+     * Method for getting the height of this BST
+     * @return height : returns the height of this BST
+     */
+    public int height() {
+        return height;
+    }
 }
 
 class Node {
