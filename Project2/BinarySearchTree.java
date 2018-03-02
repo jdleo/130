e7fd479413cddefd3e9935f2f6982369fa5a5167
John Leonardo <jdleonardo5@icloud.com>
2018-03-01 18:09:10 -0800

    node structure

--- Project2/BinarySearchTree.java
+++ Project2/BinarySearchTree.java
@@ -1,4 +1,19 @@
+/**
+ * BinarySearchTree.java
+ * @author John Leonardo, W Reed
+ * Class: CSC 130
+ */
 
 public class BinarySearchTree<Integer> {
     
+}
+
+class Node {
+    int key;
+    Node left, right;
+    
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
 }
