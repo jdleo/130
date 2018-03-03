087e021ea5fc53e28dfd39000d9ad9c1443cf546
John Leonardo <jdleonardo5@icloud.com>
2018-03-03 15:11:07 -0800

    codebase conforms to TreePrinter class references

--- Project2/BinarySearchTree.java
+++ Project2/BinarySearchTree.java
@@ -5,8 +5,8 @@
  */
 
 public class BinarySearchTree<Integer> {
-    private BinaryNode<Integer> root;      //root of BST
-    
+    private BinaryNode<Integer> root;       //root of BST
+    private int height;     //height of BST
     
     /**
      * Constructor for BinarySearchTree class
@@ -19,8 +19,16 @@ public class BinarySearchTree<Integer> {
      * Method for getting the root of this BST
      * @return root : returns the root of this BST
      */
-    public BinarySearchTree<Integer> getRoot() {
-        return root;
+    public BinaryNode<Integer> getRoot() {
+        return this.root;
+    }
+    
+    /**
+     * Method for getting the height of this BST
+     * @return height : returns the height of this BST
+     */
+    public int height() {
+        return this.height;
     }
 }
 
@@ -30,9 +38,39 @@ class BinaryNode<Integer> {
     BinaryNode<Integer> left;
     BinaryNode<Integer> right;
     
+    /**
+     * Constructor for BinaryNode class
+     * @param element : the int data to issue this node
+     * @param lt : the left child node
+     * @param rt : the right child node
+     */
     public BinaryNode(int element, BinaryNode<Integer> lt, BinaryNode<Integer> rt) {
         this.element = element;
         this.left = lt;
         this.right = rt;
+    }
+    
+    /**
+     * Method for getting the data this node contains
+     * @return element : returns the data in this node
+     */
+    public int getData() {
+        return this.element;
+    }
+    
+    /**
+     * Method for getting the left child node
+     * @return left : returns the left child node
+     */
+    public BinaryNode<Integer> getLeft() {
+        return this.left;
+    }
+    
+    /**
+     * Method for getting the right child node
+     * @return right : returns the right child node
+     */
+    public BinaryNode<Integer> getRight() {
+        return this.right;
     }
 }
