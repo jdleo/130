76832929d951da1b502fac70d8b71cca28d8f4f7
John Leonardo <jdleonardo5@icloud.com>
2018-03-04 17:51:31 -0800

    BinaryNode setup

--- Project2/BinaryNode.java
+++ Project2/BinaryNode.java
@@ -5,5 +5,70 @@
  */
 
 public class BinaryNode {
-
+    int data;        //data for this node
+    BinaryNode left;        //left child
+    BinaryNode right;       //right child
+    
+    /**
+     * Constructor
+     * @param x : data to initialize this node with
+     */
+    BinaryNode(int x) {
+        this(x, null, null);
+    }
+    
+    /**
+     * Constructor
+     * @param x : data to initialize this node with
+     * @param lt : left child
+     * @param rt : right child
+     */
+    BinaryNode(int x, BinaryNode lt, BinaryNode rt) {
+        this.data = x;
+        this.left = lt;
+        this.right = rt;
+    }
+    
+    /**
+     * Getter for data
+     * @return data
+     */
+    public int getData() {
+        return this.data;
+    }
+    
+    /**
+     * Getter for left child
+     * @return left
+     */
+    public BinaryNode getLeft() {
+        return this.left;
+    }
+    
+    /**
+     * Getter for right child
+     * @return right
+     */
+    public BinaryNode getLeft() {
+        return this.right;
+    }
+    
+    /**
+     * Setter for left child
+     * @param node : node to assign as left child
+     */
+    public void setLeft(BinaryNode node) {
+        this.left = node;
+    }
+    
+    /**
+     * Setter for right child
+     * @param node : node to assign as right child
+     */
+    public void setRight(BinaryNode node) {
+        this.right = node;
+    }
+    
+    
+    
 }
