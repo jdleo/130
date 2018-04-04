5585229145099a1e48fa75cf3c8f633b64857aa9
John Leonardo <jdleonardo5@icloud.com>
2018-04-03 19:38:37 -0700

    correlator

---
+++ Project3/Correlator.java
@@ -0,0 +1,16 @@
+/**
+ * Name: John Leonardo, W Reed
+ * Course: CSC 130
+ * File: Correlator.java
+ * Description: The main program that can be executed to count the words in a file, and print the counts in descending order.
+ */
+
+public class Correlator {
+    
+    /**
+     *  MAIN METHOD
+     */
+    public static void main(String[] args) {
+        
+    }
+}
--- Project3/WordCount.java
+++ Project3/WordCount.java
@@ -132,6 +132,9 @@ public class WordCount {
         return counts;
     }
 
+    /**
+     *  MAIN METHOD 
+     */
     public static void main(String[] args) {
         
         //string for showing usage to user
