c6a2ee9bdf6c80ef52b08348e30f8296580597cf
John Leonardo <jdleonardo5@icloud.com>
2018-04-03 19:25:36 -0700

    added better comments

--- Project3/AVLTree.java
+++ Project3/AVLTree.java
@@ -1,23 +1,39 @@
-
+/**
+ * Name: John Leonardo, W Reed
+ * Course: CSC 130
+ * File: AVLTree.java
+ * Description: This file is a AVLTree implementation of DataCounter.
+ */
 public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E> {
     
+    /**
+     * Same incCount method from BST, except we rebalance after insert
+     * @param data : the data to increase the count of (or insert)
+     */
     @Override
     public void incCount(E data) {
         super.incCount(data);
         overallRoot = balance(overallRoot);
     }
     
-    /*Balance the tree by checking height
-     *Only rebalance if the height difference is greater than 1
-     *If subtree has uneven left/right heights, perform a rotation.
+    /**
+     * Method to balance tree
+     * @param root : the parent node to rebalance from
+     * @return returns the parent node, but rebalanced now
      */
     private BSTNode balance(BSTNode root) {
+        
+        //only one node in this tree, return it
         if(root.left == null && root.right == null) {
             return root;
         }
         
+        //variable to keep track of the height difference
         int heightDiff;
+        
+        //keep trying, til the tree fully balanced
         do{
+            //difference between the heights of left and right subtrees
             heightDiff = height(root.left) - height(root.right);
             
             if(heightDiff > 1){
@@ -35,13 +51,11 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
             }
         }while(heightDiff > 1 && heightDiff < -1 && root != null);
         
+        //return the new, balanced root node
         return root;
     }
     
-    /*
-     *  This section refers to rotations that will occur
-     *  when there is a need to rebalance the tree.
-     */
+    //SINGLE-LEFT ROTATION CASE
     private BSTNode singleLeftRotation(BSTNode root) {
         BSTNode right = root.right;
         root.right = right.left;
@@ -49,6 +63,7 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
         return right;
     }
     
+    //SINGLE-RIGHT ROTATION CASE
     private BSTNode singleRightRotation(BSTNode root) {
         BSTNode left = root.left;
         root.left = left.right;
@@ -56,11 +71,13 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
         return left;
     }
     
+    //LEFT-RIGHT ROTATION CASE
     private BSTNode doubleLeftRightRotation(BSTNode root) {
         root.left = singleLeftRotation(root.left);
         return singleRightRotation(root);
     }
     
+    //RIGHT-LEFT ROTATION CASE
     private BSTNode doubleRightLeftRotation(BSTNode root) {
         root.right = singleRightRotation(root.right);
         return singleLeftRotation(root);
@@ -68,12 +85,13 @@ public class AVLTree<E extends Comparable<? super E>> extends BinarySearchTree<E
     
     private int height(BSTNode node)
     {
-        /* base case tree is empty */
+        //tree is empty, so height is 0
         if (node == null)
             return 0;
         
-        /* If tree is not empty then height = 1 + max of left
-         height and right heights */
+        //tree isn't empty
+        //the height is the maximum value between the heights
+        //of the left and right subtrees, + 1
         return 1 + Math.max(height(node.left), height(node.right));
     }
 }
