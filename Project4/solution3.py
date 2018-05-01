1ae6fe31dd9f9fda54932a8e6399842e6751cb55
John Leonardo <jdleonardo5@icloud.com>
2018-05-01 16:27:08 -0700

    solution 3

--- Project4/solution3.py
+++ Project4/solution3.py
@@ -1,1 +1,29 @@
-print("hello from solution 3")
+#!/bin/python3
+
+import sys
+
+def quickSort(arr):
+    #identify the pivot
+    pivot = arr[0]
+    
+    #create arrays for left, right, and equal
+    left = []
+    right = []
+    equal = [pivot]  #equal's only element is the pivot element
+    
+    #iterate through array
+    for item in arr:
+        if item > pivot:
+            right.append(item)
+        elif item < pivot:
+            left.append(item)
+    
+    #return left + equal + right
+    return left + equal + right
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = quickSort(arr)
+    print (" ".join(map(str, result)))
+
